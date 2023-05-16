104aaf99b484ee06ae4fdff9dc1c9fa18d85dfe0
dardosanchez <dardosanchez2000@gmail.com>
2023-05-16 19:45:54 -0300

    formulario

--- js/funciones.js
+++ js/funciones.js
@@ -1,20 +1,23 @@
 function calcularPrecio() {
+
+   
     opcion = document.getElementById("seleccion").value;
 
+    var cantEntradas = document.getElementById("cantidadEntrada").value;
     var total;
     var precioEntrada = 200;
     switch (opcion) {
         case "sinCategoria":
-            total = precioEntrada;
+            total = precioEntrada*cantEntradas;
             break;
         case "estudiante":
-            total = precioEntrada - ((precioEntrada*80)/100);
+            total = (precioEntrada - ((precioEntrada*80)/100))*cantEntradas;
             break;
         case "treinee":
-            total = precioEntrada - ((precioEntrada*50)/100);
+            total = (precioEntrada - ((precioEntrada*50)/100))*cantEntradas;
             break;
         case "junior":
-            total = precioEntrada - ((precioEntrada*15)/100);
+            total = (precioEntrada - ((precioEntrada*15)/100))*cantEntradas;
             break;
     }
 
